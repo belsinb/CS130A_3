2037086a75b8870d3eb7c9d0e35c716001dcc30e
belsinb <belsin@uail.ucsb.edu>
2014-03-03 00:44:02 -0800

    updated user input functionality

--- main.cpp
+++ main.cpp
@@ -1,22 +1,55 @@
 //the main code yo.
-#include <string>
 #include <iostream>
+#include <string>
 #include <cstdio>
 #include "minheap.h"
 #include "node.h"
 
+using namespace std;
+
+
+
 	void union(int, int);
 	int find(int);
 
-	void main()
+
+int main()
 	{
 		int n = 1;
+        int k = -5;
+        int edge_num;
+        int j;
+        
+        
+        //recieve number of nodes from user
+        cin >> n;
+        //create array of disjoint sets
+        int sets[n+1]
+        for(k=0;k<n;k++)
+        {
+            if(k=0)
+                sets[k] = -2;
+            else
+                sets[k] = -1;
+        }
+        
+        //recieve number of edges from user
+        cin >> edge_num;
+        for(k=0;k<edge_num;k++)
+        {
+            //cin >> a new node each time somehow
+        }
+        
+        
 		//get n (the number of nodes) from stdin
 
-		node *x = new node[n]();
+		//node *x = new node[n]();
 
-		return;
+		return 0;
 	}
+
+
+    
 
 	void union(int* x, int a, int b) {
 		aRoot = find(a);
--- minheap.h
+++ minheap.h
@@ -1,1 +1,5 @@
 //the minheap helper file
+#ifndef MINHEAP_H
+#define MINHEAP_H
+
+#endif
