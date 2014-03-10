12e8ca4cde2ceb16e067e801b47a7ca0e9e837ac
Evan Moelter <evanmoelter@umail.ucsb.edu>
2014-03-10 01:26:13 -0700

    implemented for loops, added TODOs, and cleaned up code

--- main.cpp
+++ main.cpp
@@ -12,12 +12,14 @@ using namespace std;
 class truple {
 public:
     int parse;
-    int getTo;
-    int getFrom;
-    int getWeight;
-    void setTo;
-    void setFrom;
-    void setWeight;
+    int getTo();
+    int getFrom();
+    int getWeight();
+    void setTo(int);
+    void setFrom(int);
+    void setWeight(int);
+    truple() = default;
+    truple(int t, int f, int w);
 private:
     int to;
     int from;
@@ -56,27 +58,30 @@ int main()
         int k = -5;
         int edge_num;
         int j;
-        int input[1200]; //why not n*3?
         string input;
 
         //recieve number of nodes from user
         cin >> n;
         //create array of disjoint sets
-        int sets[n+1]
+        /*int sets[n+1]
         for(k=0;k<n;k++)
         {
             if(k=0)
                 sets[k] = -2;
             else
                 sets[k] = -1;
-        }
+        }*/
+        int *sets = new int[n](-1); //initalize our array
+        sets[0] = 0;
 
         //recieve number of edges from user
         cin >> edge_num;
 
+        //TODO: initialize minHeap mh = new minHeap(n)
+
         //get the truples into an array called input with k = to, k+1 = from, k+2 = weight
         // why not create a truple and add it to the min heap?
-        for(k=0;k<(edge_num*3);k+=3)
+        for(k=0;k<edge_num;k++)
         {
             getline(cin, input);
 
@@ -84,13 +89,18 @@ int main()
 
             ss >> i1, i2, i3;
 
-            input[k] = i1;
-            input[k+1] = i2;
-            input[k+2] = i3;
+            mh->insert(new truple(i1, i2, i3));
+
         }
 
 
-		int *x = new int[n](-1); //initalize our array
+
+        // pop out truple from minheap
+        for(k=1;k<edge_num;k++) {
+            truple *t = mh->pop();
+            union(sets, t->getTo(), t->getFrom());
+        }
+
 
 		return 0;
 	}
@@ -102,11 +112,11 @@ int main()
         int aRoot = find(a);
 		int bRoot = find(b);
 
-		if (aRoot == bRoot)
+		if (aRoot > 0 && aRoot == bRoot)
 			return; //a and b are already in the same set
 
         // set the smaller root to the bigger one and add their sizes
-		if (x[aRoot] < x[bRoot]) {
+		if (x[aRoot] <= x[bRoot]) {
             x[aRoot] += x[bRoot];
             x[bRoot] = aRoot;
         }
@@ -114,6 +124,8 @@ int main()
             x[bRoot] += x[aRoot];
             x[aRoot] = bRoot;
         }
+
+        //TODO: print to stdout
 }
 
     int find(int *x, int a) {
